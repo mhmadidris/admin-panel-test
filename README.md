0d0de3c5f9c52b1545fa7f8eec16eb3ffd1d5245
mhmadidris <idris3097@gmail.com>
2024-05-17 11:16:40 +0700

    Finishing v2

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# admin-panel-test
+# Admin Panel Website
+
+## Instructions:
+
+#### Install Laravel Project
+
+1.  clone this repository
+2.  run `npm install` and `npm run build`
+3.  run composer install `composer install`
+4.  duplicate the example .env file, and rename the duplicate file to .env
+5.  configure DB_CONNECTION, DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD in .env file (see configure database in database section)
+6.  create database in pgAdmin (if using postgreSQL) / phpMyAdmin (if using MySQL) and create database name "admindb"
+7.  run `php artisan migrate`
+8.  run `php artisan db:seed`
+9.  run `php artisan key:generate`
+10. run `php artisan serve`
+
+## Configure Database
+
+#### PosgreSQL
+
+```
+DB_CONNECTION=pgsql
+DB_HOST=127.0.0.1
+DB_PORT=5432
+DB_DATABASE=admindb
+DB_USERNAME=
+DB_PASSWORD=
+```
+
+#### MySQL
+
+```
+DB_CONNECTION=mysql
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DB_DATABASE=admindb
+DB_USERNAME=
+DB_PASSWORD=
+```
+
+## Dummy Account
+
+**Admin** <br />
+Email: admin@mail.com <br />
+Password: admin123
+<br />
+<br />
+**Customer** <br />
+Email: customer@mail.com <br />
+Password: customer123
+
+## Plugin/Libraries/Template:
+
+-   Laravel
+-   CoreUI
+-   Laratrust
+-   Livewire
+-   SweetAlert
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,52 +6,38 @@
         </a>
     </li>
 
-    <li class="nav-group" aria-expanded="false">
-        <a class="nav-link nav-group-toggle text-white" href="#">
-            <i class="nav-icon fas fa-tags"></i>
-            Atribut
-        </a>
-        <ul class="nav-group-items">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('brand.index', ['locale' => app()->getLocale()]) }}">
-                    {{ __('Merk') }}
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('category.index', ['locale' => app()->getLocale()]) }}">
-                    {{ __('Kategori') }}
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('unit.index', ['locale' => app()->getLocale()]) }}">
-                    {{ __('Satuan') }}
-                </a>
-            </li>
-        </ul>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('catalog.index', ['locale' => app()->getLocale()]) }}">
-            <i class="nav-icon fa-solid fa-bag-shopping"></i>
-            {{ __('Katalog') }}
-        </a>
-    </li>
-
-    @if (Auth::user() && Auth::user()->hasRole('director'))
+    @if (Auth::user()->hasRole('customer'))
         <li class="nav-group" aria-expanded="false">
-            <a class="nav-link nav-group-toggle" href="#">
-                <i class="nav-icon fas fa-cog"></i>
-                Pengaturan
+            <a class="nav-link nav-group-toggle text-white" href="#">
+                <i class="nav-icon fas fa-tags"></i>
+                Atribut
             </a>
             <ul class="nav-group-items">
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('setting-admin.index', ['locale' => app()->getLocale()]) }}">
-                        {{ __('Pengaturan Peran') }}
+                    <a class="nav-link" href="{{ route('brand.index', ['locale' => app()->getLocale()]) }}">
+                        {{ __('Merk') }}
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('category.index', ['locale' => app()->getLocale()]) }}">
+                        {{ __('Kategori') }}
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('unit.index', ['locale' => app()->getLocale()]) }}">
+                        {{ __('Satuan') }}
                     </a>
                 </li>
             </ul>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('catalog.index', ['locale' => app()->getLocale()]) }}">
+                <i class="nav-icon fa-solid fa-bag-shopping"></i>
+                {{ __('Katalog') }}
+            </a>
         </li>
     @endif
 </ul>
